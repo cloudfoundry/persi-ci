9d860e1c7f2a21442ba7dd2970d24d58228f83b0
Julian Hjortshoj <julian.hjortshoj@emc.com>
2017-03-22 11:23:34 -0700

    set ldap connection parameters on nfsv3driver job in runtime-config [#134456969](https://www.pivotaltracker.com/story/show/134456969)

--- scripts/ci/update_runtime_config.sh
+++ scripts/ci/update_runtime_config.sh
@@ -1,2 +1,38 @@
 #!/usr/bin/env bash
-echo "This is a no-op"
+
+cat runtime-config-out/addons.yml << EOF
+---
+releases:
+- name: local-volume
+  version: TBD
+- name: cephfs
+  version: TBD
+- name: efs-volume
+  version: TBD
+- name: nfs-volume
+  version: TBD
+addons:
+- name: voldrivers
+  include:
+    deployments: [persi-cf-diego]
+    jobs: [{name: rep, release: diego}]
+  jobs:
+  - name: localdriver
+    release: local-volume
+    properties: {}
+  - name: cephdriver
+    release: cephfs
+    properties: {}
+  - name: efsdriver
+    release: efs-volume
+    properties: {}
+  - name: nfsv3driver
+    release: nfs-volume
+    properties:
+      ldap_svc_user: ${LDAP_SVC_USER}
+      ldap_svc_password: ${LDAP_SVC_PASS}
+      ldap_host: ${LDAP_HOST}
+      ldap_port: ${LDAP_PORT}
+      ldap_proto: ${LDAP_PROTO}
+      ldap_user_fqdn: ${LDAP_USER_FQDN}
+EOF
