6b548f8d621bfc374dabf7ca62d1093621253027
Jeff Pak <jeffrey.pak@emc.com>
2017-05-25 16:23:11 -0700

    update broker creation job to use broker bosh release [#146031523](https://www.pivotaltracker.com/story/show/146031523)
    Signed-off-by: Paul Warren <paul.warren@emc.com>

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -4,5 +4,8 @@
 
 set -x -e
 
+pushd ecs-broker-boshrelease
+
 persi-ci/scripts/ci/bosh_setup
-bosh -d deployments-persi/ecs-manifest.yml -n run errand deploy-service-broker
+bosh -n create release --name ecs-broker-boshrelease --force && bosh -n upload release
+bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
