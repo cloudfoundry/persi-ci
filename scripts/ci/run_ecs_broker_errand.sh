da8452d92465a056cf8676bdaa8a5d4fa9654b08
Jeff Pak <jeffrey.pak@emc.com>
2017-05-26 09:57:42 -0700

    forgot the popd

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -8,4 +8,5 @@ persi-ci/scripts/ci/bosh_setup
 
 pushd ecs-broker-boshrelease
 bosh -n create release --name ecs-broker-boshrelease --force && bosh -n upload release
+popd
 bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
