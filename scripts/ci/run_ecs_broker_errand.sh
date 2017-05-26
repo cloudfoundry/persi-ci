b29a1a6e796c7f8c7b22e10c8a20a9f66719da5e
Paul Warren <paul.warren@emc.com>
2017-05-26 09:53:19 -0700

    Fix order in broker script
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -4,8 +4,8 @@
 
 set -x -e
 
-pushd ecs-broker-boshrelease
-
 persi-ci/scripts/ci/bosh_setup
+
+pushd ecs-broker-boshrelease
 bosh -n create release --name ecs-broker-boshrelease --force && bosh -n upload release
 bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
