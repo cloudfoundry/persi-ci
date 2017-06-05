bd8870ae82f529fc71c46225394f60bb17c27768
Jeff Pak <jeffrey.pak@emc.com>
2017-06-05 16:07:14 -0700

    update ecs broker deployment to use ecs release instead of bosh release tarball

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -6,7 +6,7 @@ set -x -e
 
 persi-ci/scripts/ci/bosh_setup
 
-pushd ecs-broker-boshrelease
-bosh -n create release --name ecs-broker-boshrelease --force && bosh -n upload release
+pushd ecs-release
+bosh -n create release --name ecs-release --force && bosh -n upload release
 popd
-bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
+bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-broker
