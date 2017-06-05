3524defdcdd00a7f4e90c191d5c1a17610599c98
Jeff Pak <jeffrey.pak@emc.com>
2017-06-05 16:18:05 -0700

    change errand name to deploy-service-broker for ecs

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -9,4 +9,4 @@ persi-ci/scripts/ci/bosh_setup
 pushd ecs-release
 bosh -n create release --name ecs-release --force && bosh -n upload release
 popd
-bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-broker
+bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
