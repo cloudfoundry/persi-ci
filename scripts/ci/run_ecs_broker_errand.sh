2cd3b24fce096472653c54cf1fe62e75aa03f602
Julian Hjortshoj <julian.hjortshoj@emc.com>
2017-06-06 11:03:22 -0700

    fix broker errand
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- scripts/ci/run_ecs_broker_errand.sh
+++ scripts/ci/run_ecs_broker_errand.sh
@@ -6,7 +6,7 @@ set -x -e
 
 persi-ci/scripts/ci/bosh_setup
 
-pushd ecs-release
-bosh -n create release --name ecs-release --force && bosh -n upload release
-popd
-bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-service-broker
+#pushd ecs-release
+# bosh -n create release --name ecs-release --force && bosh -n upload release && bosh -n deploy
+#popd
+bosh -d deployments-persi/ecs-broker-aws-ec2-manifest.yml -n run errand deploy-broker
