681d8c62e3b5ca6e2aa9073f955e23a2d35d4905
Jeffrey Pak <jeffrey.pak@emc.com>
2017-07-24 16:43:22 -0700

    add scripts and job for creating proto [#149091715](https://www.pivotaltracker.com/story/show/149091715)

---
+++ scripts/ci/generate-proto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd csi-spec
+  go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+  export PATH=$PATH:$HOME/src/go/bin
+  make csi.proto
+  make csi.pb.go
+  mv csi.pb.go ../created-csi-proto
+popd
+
