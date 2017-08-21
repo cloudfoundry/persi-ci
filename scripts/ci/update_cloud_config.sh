fddb046d7f2b859f2ff17dc114cc36f09ad275df
Jeff Pak <jeffrey.pak@emc.com>
2017-08-21 15:47:21 -0700

    whoops
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -4,4 +4,4 @@ bbl --state-dir director-state print-env > bosh-env/set-env.sh
 source bosh-env/set-env.sh
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
-bosh update-cloud-config temp2.yml
+bosh -n update-cloud-config temp2.yml
