a3f27f4ec0d60b2ae5941082eecc13206f2873e9
Kai Xiang <kxiang@pivotal.io>
2017-10-03 16:16:16 -0700

    update cloud config with iam-instance-profile
    [151625351](https://www.pivotaltracker.com/story/show/151625351)
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -3,5 +3,5 @@
 bbl --state-dir director-state print-env > set-env.sh
 source set-env.sh
 bosh cloud-config > temp.yml
-bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
+bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml -o persi-ci/operations/use-efsbroker-for-iam-instance-profile.yml > temp2.yml
 bosh -n update-cloud-config temp2.yml
