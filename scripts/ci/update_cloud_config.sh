34640dc70da4bd6eac5a47675220b22464bf9dc8
Kai Xiang <kxiang@pivotal.io>
2017-10-10 16:12:05 -0700

    make it work with latest cf-deployment

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -2,6 +2,7 @@
 
 bbl --state-dir director-state print-env > set-env.sh
 source set-env.sh
+unset BOSH_ALL_PROXY
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
 bosh -n update-cloud-config temp2.yml
