9f5b3b1befb10eb50cd72d1d077fb5e6b0e4b743
Julian Hjortshoj <julian.hjortshoj@emc.com>
2018-01-29 15:32:24 -0800

    minor scripty tweaks

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e -x -u
+
 bbl --state-dir director-state/bbl-state print-env > set-env.sh
 source set-env.sh
+sleep 2
 
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
