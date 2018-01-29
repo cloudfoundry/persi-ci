801e34f868960a8bdadb74ec0ccb0f636d9bed39
Julian Hjortshoj <julian.hjortshoj@emc.com>
2018-01-29 10:23:54 -0800

    fix hang in bosh client

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -2,7 +2,7 @@
 
 bbl --state-dir director-state/bbl-state print-env > set-env.sh
 source set-env.sh
-unset BOSH_ALL_PROXY
+
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
 bosh -n update-cloud-config temp2.yml
