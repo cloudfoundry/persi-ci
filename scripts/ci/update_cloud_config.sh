6f78e044aa803d64a2695361f9030f90d30623ed
Julian Hjortshoj <julian.hjortshoj@emc.com>
2018-01-29 16:53:02 -0800

    kill bbl local bosh proxy at exit so that concourse jobs don't hang

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -2,10 +2,13 @@
 
 set -e -x -u
 
+function finish {
+  pkill ssh || true
+}
+trap finish EXIT
+
 bbl --state-dir director-state/bbl-state print-env > set-env.sh
 source set-env.sh
-sleep 2
-
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml > temp2.yml
 bosh -n update-cloud-config temp2.yml
