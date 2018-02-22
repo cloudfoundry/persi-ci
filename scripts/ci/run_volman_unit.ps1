d37e61bcde164df3f472c24cc60af6d281d3e830
Kai Xiang <kxiang@pivotal.io>
2018-02-22 12:05:10 -0800

    Add volman/voldriver unit test running on windows
    [##155268532](https://www.pivotaltracker.com/story/show/#155268532)
    
    Signed-off-by: Paul Warren <paul.warren@emc.com>

---
+++ scripts/ci/run_volman_unit.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+
+cd diego-release
+
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd src/code.cloudfoundry.org/volman
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+
+cd ../voldriver
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
