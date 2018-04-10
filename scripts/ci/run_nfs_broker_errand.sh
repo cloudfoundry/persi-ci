a8e750655c9a0e454a3f842575264deb72ec7d82
Julian Hjortshoj <julian.hjortshoj@emc.com>
2018-04-10 10:11:09 -0700

    pipeline changes to push nfs broker with an errand instead of bosh deploying it for all flows [#155384701](https://www.pivotaltracker.com/story/show/155384701)

---
+++ scripts/ci/run_nfs_broker_errand.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x -e
+
+function finish {
+  pkill ssh || true
+}
+trap finish EXIT
+
+bbl --state-dir bbl-state/bbl-state print-env > set-env.sh
+source set-env.sh
+
+bosh -n -d cf run-errand nfsbrokerpush
