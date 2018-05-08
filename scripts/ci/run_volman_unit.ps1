e50419eb07010c99aed1e53acf0b0d69423b9d4f
Maria Shaldibina <mshaldibina@pivotal.io>
2018-05-08 14:26:49 -0700

    volman windows unit tests exit 1 when ginkgo fails

--- scripts/ci/run_volman_unit.ps1
+++ scripts/ci/run_volman_unit.ps1
@@ -1,9 +1,17 @@
 ﻿$ErrorActionPreference = “Stop”;
 trap { $host.SetShouldExit(1) }
 
+function CheckLastExitCode {
+  if ($LastExitCode -ne 0) {
+    Write-Host "Unit tests failed"
+    exit 1
+  } else {
+    Write-Host "Unit tests passed"
+    exit 0
+  }
+}
 
 cd diego-release
-
 
 $env:GOPATH=$PWD
 $env:PATH="$PWD/bin;$env:PATH"
@@ -12,6 +20,8 @@ go install github.com/onsi/ginkgo/ginkgo
 
 cd src/code.cloudfoundry.org/volman
 ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+CheckLastExitCode
 
 cd ../voldriver
 ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+CheckLastExitCode
