8d8f2ec553edd9833e597f91db90ca16f123b9ee
Kai Xiang <kxiang@pivotal.io>
2018-05-17 16:07:09 -0700

    Add BBL_STATE_DIR for update_cloud_config task
    [#157379255](https://www.pivotaltracker.com/story/show/157379255)

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -7,7 +7,7 @@ function finish {
 }
 trap finish EXIT
 
-bbl --state-dir director-state/bbl-state print-env > set-env.sh
+bbl --state-dir director-state/${BBL_STATE_DIR} print-env > set-env.sh
 source set-env.sh
 bosh cloud-config > temp.yml
 bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml -o persi-ci/operations/change-minimal-vm-to-t2-nano.yml > temp2.yml
