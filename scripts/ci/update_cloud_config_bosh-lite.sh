0516f4af6cadbf5c907952de7ae13215abef4862
Maria Shaldibina <pivotal.io>
2018-05-18 12:03:34 -0700

    Add a task to update bosh-lite cloud config
    Signed-off-by: Paul Warren <paul.warren@emc.com>

---
+++ scripts/ci/update_cloud_config_bosh-lite.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e -x -u
+
+source persi-ci/efs/shared-functions
+
+trap teardown_bosh_env_vars EXIT
+
+setup_bosh_env_vars_with_ssh
+bosh -n update-cloud-config bosh-deployment/warden/cloud-config.yml
+teardown_bosh_env_vars
