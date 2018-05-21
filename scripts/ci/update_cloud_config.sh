a73fdab1d87c3fb97ca010f6f875d0bfab46359a
Kai Xiang <kxiang@pivotal.io>
2018-05-21 10:52:19 -0700

    Don't apply aws ops file for gcp
    [#157379255](https://www.pivotaltracker.com/story/show/157379255)

--- scripts/ci/update_cloud_config.sh
+++ scripts/ci/update_cloud_config.sh
@@ -10,5 +10,13 @@ trap finish EXIT
 bbl --state-dir director-state/${BBL_STATE_DIR} print-env > set-env.sh
 source set-env.sh
 bosh cloud-config > temp.yml
-bosh interpolate temp.yml -o persi-ci/operations/add-vip-network-to-bosh.yml -o persi-ci/operations/change-minimal-vm-to-t2-nano.yml > temp2.yml
+
+if [ $BBL_IAAS -eq "aws" ]
+then
+ ops_flag="-o persi-ci/operations/add-vip-network-to-bosh.yml -o persi-ci/operations/change-minimal-vm-to-t2-nano.yml"
+else
+ ops_flag=""
+fi
+
+bosh interpolate temp.yml $ops_flag > temp2.yml
 bosh -n update-cloud-config temp2.yml
