0507c0c9e6dd0d7e2a74801667fbc01151854b69
Dave Walter <dwalter@pivotal.io>
2018-11-07 12:20:11 -0800

    Update run-pats to use a standalone PATs repo
    - as opposed to one submoduled into a release repo
    - removed the recursive flag from the ginkgo parameters passed to
    bin/test now that persi-acceptance-tests has submodules with tests
    
    [#161729672](https://www.pivotaltracker.com/story/show/161729672)

--- scripts/ci/run-pats.sh
+++ scripts/ci/run-pats.sh
@@ -10,19 +10,17 @@ fi
 export CONFIG="${PWD}/${CONFIG_FILE_PATH}"
 
 set +x
-${PWD}/persi-ci/scripts/ci/bbl_get_bosh_env
+"${PWD}/persi-ci/scripts/ci/bbl_get_bosh_env"
 source bosh-env/set-env.sh
 set -x
 
-pushd release
+pushd persi-acceptance-tests
   export GOPATH="${PWD}"
   export PATH="${GOPATH}/bin:${PATH}"
 
-  pushd  src/code.cloudfoundry.org/persi-acceptance-tests
-    if [[ -n "${PARALLEL_NODES}" ]]; then
-      ./bin/test --slowSpecThreshold=300 -r -nodes "${PARALLEL_NODES}" .
-    else
-      ./bin/test --slowSpecThreshold=300 -r -p .
-    fi
-  popd
+  if [[ -n "${PARALLEL_NODES}" ]]; then
+    ./bin/test --slowSpecThreshold=300 -nodes "${PARALLEL_NODES}" .
+  else
+    ./bin/test --slowSpecThreshold=300 -p .
+  fi
 popd
