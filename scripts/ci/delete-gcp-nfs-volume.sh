d2651b34e2ae351d988e893e2f014ebd8adb3a08
Dennis Leon <dleon@pivotal.io>
2019-05-10 11:47:55 -0700

    remove unnecessary concourse tasks now that nfsdriver-certification uses its own (ganesha) nfs server [#165872817](https://www.pivotaltracker.com/story/show/165917690)
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>
    Co-authored-by: Julian Hjortshoj <julian.hjortshoj@emc.com>

--- scripts/ci/delete-gcp-nfs-volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-echo "${GCP_SERVICE_ACCOUNT_KEY}" | gcloud auth activate-service-account --key-file=-
-
-gcloud beta filestore instances delete "${FILESTORE_INSTANCE_NAME}" \
-  --location="${GCP_LOCATION}" \
-  --project "${GCP_PROJECT} \
-  -q
