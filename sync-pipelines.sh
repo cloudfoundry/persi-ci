f878562fdf9e8448d095cd2988d56da8b0fba4f9
Lisa Burns <lcho@pivotal.io>
2020-03-11 09:50:55 -0700

    Oops typo
    Authored-by: Lisa Burns <lcho@pivotal.io>

--- sync-pipelines.sh
+++ sync-pipelines.sh
@@ -90,7 +90,7 @@ jobs:
       mapfs-tag: v1.2.0
       nfs-semver-initial-version: 5.0.3
       pas-version: us_2_8
-  - set_pipeline: lts-2.3
+  - set_pipeline: lts-toolsmiths-2.3
     file: persi-ci/lts-toolsmiths.yml
     vars:
       <<: *lastpass_creds
