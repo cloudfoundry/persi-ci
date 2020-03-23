ffacb97f15cfb3dc9a3215e27d8daeee0ecb4e78
Dennis Leon <leonde@vmware.com>
2020-03-23 16:47:02 -0700

    Drats should genearte config using smith
    - export the overriden bbl function
    - fetch the jumpbox-address from the smith metadata file
    
    [#171822159](https://www.pivotaltracker.com/story/show/171822159)
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- scripts/ci/generate-drats-integration-config.sh
+++ scripts/ci/generate-drats-integration-config.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+wget https://github.com/cloudfoundry/bosh-bootloader/releases/download/v8.4.0/bbl-v8.4.0_linux_x86-64
+chmod +x bbl-v8.4.0_linux_x86-64
+mv bbl-v8.4.0_linux_x86-64 `which bbl`
+
 export SCRIPTS_ROOT_DIR="${PWD}"
 export BBL_PATH="$(which bbl)"
 
 bbl() {
   for last; do true; done
-  $BBL_PATH --metadata-file ${SCRIPTS_ROOT_DIR}/smith-env/metadata $last
+  if [ $last = "jumpbox-address" ]; then
+    cat ${SCRIPTS_ROOT_DIR}/smith-env/metadata | jq ".bosh.bosh_all_proxy" | sed -E 's#.*jumpbox@(.*)\?.*#\1#'
+  else
+    $BBL_PATH print-env --metadata-file ${SCRIPTS_ROOT_DIR}/smith-env/metadata
+  fi
 }
 
 
@@ -13,5 +21,6 @@ ENV=`cat smith-env/metadata | jq -r ".name"`
 export APPS_DOMAIN="${ENV}.cf-app.com"
 export CF_API_ENDPOINT="api.${ENV}.cf-app.com"
 export SYSTEM_DOMAIN="${ENV}.cf-app.com"
+export -f bbl
 
 $SCRIPTS_ROOT_DIR/disaster-recovery-acceptance-tests/ci/credhub-compatible/update-integration-config/task.sh
