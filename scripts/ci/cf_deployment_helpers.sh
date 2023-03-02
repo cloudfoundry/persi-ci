bef4b7e6ec2c07b6f41975c6a5487076fbe317b6
Gareth Smith <sgareth@vmware.com>
2023-03-02 14:54:26 +0000

    Move smith image used by the pats task to the cryogenics/essentials-cf6 image
    Correct error handling in cf-deployment-helpers so that failures are seen in concourse
    
    Signed-off-by: Iain Findlay <fiain@vmware.com>

--- scripts/ci/cf_deployment_helpers.sh
+++ scripts/ci/cf_deployment_helpers.sh
@@ -1,11 +1,13 @@
 function set_cf_admin_password() {
     BOSH_ENV_NAME=$(cat smith-env/name)
     eval "$(smith -e $BOSH_ENV_NAME bosh)"
-    export CF_ADMIN_PASSWORD=$(credhub find -j -n "cf_admin_password" | jq -r '.credentials[].name' | xargs credhub get -j -n | jq -r '.value')
+    CF_ADMIN_PASSWORD=$(credhub find -j -n "cf_admin_password" | jq -r '.credentials[].name' | xargs credhub get -j -n | jq -r '.value')
+    export CF_ADMIN_PASSWORD
 }
 
 function set_cf_api_url() {
-    export CF_API_ENDPOINT=$(cat smith-env/metadata | jq -r '.cf.api_url')
+    CF_API_ENDPOINT=$(cat smith-env/metadata | jq -r '.cf.api_url')
+    export CF_API_ENDPOINT
 }
 
 function set_apps_domain() {
