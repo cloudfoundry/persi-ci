75726653fdea019cd2e216f55f137beffd40a9ab
ifindlay-cci <fiain@vmware.com>
2023-03-10 09:47:52 +0000

    Revert "Fixed error handling in cf-deployment-helpers scripts so that errors aren't hidden"
    This reverts commit 6b07f5f6cb4c42ee1f367ab120823ddf7e71080a.

--- scripts/ci/cf_deployment_helpers.sh
+++ scripts/ci/cf_deployment_helpers.sh
@@ -1,7 +1,6 @@
 function set_cf_admin_password() {
     BOSH_ENV_NAME=$(cat smith-env/name)
-    BOSH_PARAMS="$(smith -e $BOSH_ENV_NAME bosh)"
-    eval $BOSH_PARAMS
+    eval "$(smith -e $BOSH_ENV_NAME bosh)"
     CF_ADMIN_PASSWORD=$(credhub find -j -n "cf_admin_password" | jq -r '.credentials[].name' | xargs credhub get -j -n | jq -r '.value')
     export CF_ADMIN_PASSWORD
 }
