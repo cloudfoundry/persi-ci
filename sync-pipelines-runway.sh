5029979532b6ae3868e90a785fab3b051533f7b7
Diego Lemos <dlemos@vmware.com>
2023-10-16 17:08:35 +0100

    Add linter to add pipelines

--- sync-pipelines-runway.sh
+++ sync-pipelines-runway.sh
@@ -4,6 +4,8 @@ set -x
 fly -t runway-cryogenics set-pipeline -p keep-volume-service-pipelines-sync -c <(
 cat <<EOF
 
+github_ssh_key: &github_ssh_key ((github.ssh_key))
+
 resources:
 - name: persi-ci
   type: git
@@ -11,44 +13,214 @@ resources:
     uri: https://github.com/cloudfoundry/persi-ci.git
     branch: master
 
+- name: cryogenics-concourse-tasks
+  type: git
+  icon: github
+  source:
+    uri: git@github.com:pivotal/cryogenics-concourse-tasks.git
+    private_key: *github_ssh_key
+
 jobs:
-- name: reconfigure
+- name: reset-docker-image-build
   plan:
-  - get: persi-ci
-    trigger: true
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/docker-image-build.yml
   - set_pipeline: volume-services-docker-image-build
     file: persi-ci/docker-image-build.yml
+- name: reset-nfs-volume-release
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/nfs-volume-release.yml
   - set_pipeline: nfs-volume-release
     file: persi-ci/nfs-volume-release.yml
+- name: reset-mapfs-release
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/mapfs-release.yml
   - set_pipeline: mapfs-release
     file: persi-ci/mapfs-release.yml
+- name: reset-smb-volume-release
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/smb-volume-release.yml
   - set_pipeline: smb-volume-release
     file: persi-ci/smb-volume-release.yml
+- name: reset-mapfs
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/mapfs.yml
   - set_pipeline: mapfs
     file: persi-ci/mapfs.yml
+- name: reset-nfsbroker
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/nfsbroker.yml
   - set_pipeline: nfsbroker
     file: persi-ci/nfsbroker.yml
+- name: reset-nfsv3driver
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/nfsv3driver.yml
   - set_pipeline: nfsv3driver
     file: persi-ci/nfsv3driver.yml
-#  - set_pipeline: norsk
-#    file: persi-ci/norsk.yml
+- name: reset-shared-units
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/shared-units.yml
   - set_pipeline: shared-units
     file: persi-ci/shared-units.yml
+- name: reset-smbbroker
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/smbbroker.yml
   - set_pipeline: smbbroker
     file: persi-ci/smbbroker.yml
+- name: reset-smbdriver
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/smbdriver.yml
   - set_pipeline: smbdriver
     file: persi-ci/smbdriver.yml
-#  - set_pipeline: lts-nfs-volume-release-v5.0
-#    file: persi-ci/lts-nfs-volume-release-v5.0.yml
-#    vars:
-#      lts-nfs-branch: v5.0
-#      mapfs-tag: v1.2.0
-#      nfs-semver-initial-version: 5.0.3
-#      pas-version: us_2_11_lts2
+# - name: reset-lts-nfs-volume-release-v5.0
+#   plan:
+#   - in_parallel:
+#     - get: persi-ci
+#       trigger: true
+#     - get: cryogenics-concourse-tasks
+#   - task: check-pipeline-for-stray-secrets
+#     file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+#     input_mapping:
+#       cryogenics-concourse-tasks: cryogenics-concourse-tasks
+#       pipeline-repo: persi-ci
+#     params:
+#       PIPELINE_TO_CHECK: ./pipeline-repo/lts-nfs-volume-release-v5.0.yml
+#   - set_pipeline: lts-nfs-volume-release-v5.0
+#     file: persi-ci/lts-nfs-volume-release-v5.0.yml
+#     vars:
+#       lts-nfs-branch: v5.0
+#       mapfs-tag: v1.2.0
+#       nfs-semver-initial-version: 5.0.3
+#       pas-version: us_2_11_lts2
+- name: reset-lts-nfsbroker-v5.0
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/lts-nfsbroker-v5.0.yml
   - set_pipeline: lts-nfsbroker-v5.0
     file: persi-ci/lts-nfsbroker-v5.0.yml
     vars:
       lts-nfs-branch: v5.0
+- name: reset-lts-nfsv3driver-v5.0
+  plan:
+  - in_parallel:
+    - get: persi-ci
+      trigger: true
+    - get: cryogenics-concourse-tasks
+  - task: check-pipeline-for-stray-secrets
+    file: cryogenics-concourse-tasks/pipeline-linting/check-pipeline-for-stray-secrets/task.yml
+    input_mapping:
+      cryogenics-concourse-tasks: cryogenics-concourse-tasks
+      pipeline-repo: persi-ci
+    params:
+      PIPELINE_TO_CHECK: ./pipeline-repo/lts-nfsv3driver-v5.0.yml
   - set_pipeline: lts-nfsv3driver-v5.0
     file: persi-ci/lts-nfsv3driver-v5.0.yml
     vars:
