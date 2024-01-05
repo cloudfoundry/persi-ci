2d3e3e6e106179634a6fb9a52debe45cdb9f787e
Diego Lemos <dlemos@vmware.com>
2024-01-05 15:58:12 +0000

    [ci] Get system domain from lock file
    [#186767842]

--- scripts/ci/cf_deployment_helpers_shp.sh
+++ scripts/ci/cf_deployment_helpers_shp.sh
@@ -9,7 +9,6 @@ function set_cf_api_url() {
 }
 
 function set_apps_domain() {
-    BOSH_ENV_NAME=$(jq -r '.name' smith-env/metadata)
-    APPS_DOMAIN="${BOSH_ENV_NAME}.cf-app.com"
+    APPS_DOMAIN="$(jq -r '.cf.api_url | capture("^api\\.(?<system_domain>.*)$") | .system_domain' smith-env/metadata)"
     export APPS_DOMAIN
 }
